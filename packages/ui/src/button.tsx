dcd253364a1405758244a4e9f4cec71254ce27a7
fahmialis <fahmi@nusantara.technology>
2024-08-18 22:18:16 +0700

    add antd button to ui for possible future styling

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react';
-import '../styles/globals.css';
-
+import { Button as AntdButton } from 'antd';
 export interface ButtonProps {
   children: ReactNode;
-  // className?: string;
 }
 
 export const Button = ({ children }: ButtonProps) => {
-  return <button className="bg-green-800">{children}</button>;
+  return <AntdButton>{children}</AntdButton>;
 };
